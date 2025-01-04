2668a8a1c77d82acfc6451b7a1cd91acd7545ded
masoud-tesna <masoud_tesna@live.com>
2025-01-04 12:12:10 +0330

    change getBase64 function

--- src/get-base64.ts
+++ src/get-base64.ts
@@ -1,5 +1,11 @@
-export const getBase64 = <T = Blob>(img: T, callback: (url: string) => void) => {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result as string));
-  reader.readAsDataURL(img as Blob);
-};
+interface TFile extends File {
+  uid: string;
+}
+
+export const getBase64 = <T = TFile>(file: T): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file as TFile);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
