ef4baf668545c6ffa406e4f2b1b1266ac7d4614d
masoud <masoud_tesna@live.com>
2025-03-06 23:48:41 +0330

    fix isArrayEqual use lodash

--- src/is-array-equal.ts
+++ src/is-array-equal.ts
@@ -1,4 +1,6 @@
-import { isEmpty, isEqual, size, xorWith } from "lodash";
+import size from 'lodash.size'
+import isEmpty from 'lodash.isempty'
+import xorWith from 'lodash.xorwith'
+import isEqual from 'lodash.isequal'
 
-export const isArrayEqual = (x: unknown[], y: unknown[]) =>
-  size(x) === size(y) && isEmpty(xorWith(x, y, isEqual));
+export const isArrayEqual = (x: unknown[], y: unknown[]) => size(x) === size(y) && isEmpty(xorWith(x, y, isEqual))
