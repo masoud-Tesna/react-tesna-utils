c78be394c7b4c4c93a24db237171aa4e08e13174
masoud <masoud_tesna@live.com>
2025-04-18 00:20:53 +0330

    fix type error

---
+++ src/custom.d.ts
@@ -0,0 +1,4 @@
+declare module 'lodash.size'
+declare module 'lodash.isempty'
+declare module 'lodash.xorwith'
+declare module 'lodash.isequal'
--- src/use-lock-body-scroll.ts
+++ src/use-lock-body-scroll.ts
@@ -4,12 +4,15 @@ import { isBrowser, off, on } from './util'
 export function getClosestBody(el: Element | HTMLElement | HTMLIFrameElement | null): HTMLElement | null {
   if (!el) {
     return null
-  } else if (el.tagName === 'BODY') {
+  }
+  else if (el.tagName === 'BODY') {
     return el as HTMLElement
-  } else if (el.tagName === 'IFRAME') {
+  }
+  else if (el.tagName === 'IFRAME') {
     const document = (el as HTMLIFrameElement).contentDocument
     return document ? document.body : null
-  } else if (!(el as HTMLElement).offsetParent) {
+  }
+  else if (!(el as HTMLElement).offsetParent) {
     return null
   }
 
@@ -43,74 +46,79 @@ let documentListenerAdded = false
 export default !doc
   ? function useLockBodyMock(_locked: boolean = true, _elementRef?: RefObject<HTMLElement>) {}
   : function useLockBody(locked: boolean = true, elementRef?: RefObject<HTMLElement>) {
-      const bodyRef = useRef(doc!.body)
-      elementRef = elementRef || bodyRef
+    const bodyRef = useRef(doc!.body)
+    elementRef = elementRef || bodyRef
 
-      const lock = (body) => {
-        const bodyInfo = bodies.get(body)
-        if (!bodyInfo) {
-          bodies.set(body, { counter: 1, initialOverflow: body.style.overflow })
-          if (isIosDevice) {
-            if (!documentListenerAdded) {
-              on(document, 'touchmove', preventDefault, { passive: false })
+    const lock = (body: HTMLElement) => {
+      const bodyInfo = bodies.get(body)
+      if (!bodyInfo) {
+        bodies.set(body, { counter: 1, initialOverflow: body.style.overflow })
+        if (isIosDevice) {
+          if (!documentListenerAdded) {
+            on(document, 'touchmove', preventDefault, { passive: false })
 
-              documentListenerAdded = true
-            }
-          } else {
-            body.style.overflow = 'hidden'
+            documentListenerAdded = true
           }
-        } else {
+        }
+        else {
+          body.style.overflow = 'hidden'
+        }
+      }
+      else {
+        bodies.set(body, {
+          counter: bodyInfo.counter + 1,
+          initialOverflow: bodyInfo.initialOverflow,
+        })
+      }
+    }
+
+    const unlock = (body: HTMLElement) => {
+      const bodyInfo = bodies.get(body)
+      if (bodyInfo) {
+        if (bodyInfo.counter === 1) {
+          bodies.delete(body)
+          if (isIosDevice) {
+            body.ontouchmove = null
+
+            if (documentListenerAdded) {
+              off(document, 'touchmove', preventDefault)
+              documentListenerAdded = false
+            }
+          }
+          else {
+            body.style.overflow = bodyInfo.initialOverflow
+          }
+        }
+        else {
           bodies.set(body, {
-            counter: bodyInfo.counter + 1,
+            counter: bodyInfo.counter - 1,
             initialOverflow: bodyInfo.initialOverflow,
           })
         }
       }
-
-      const unlock = (body) => {
-        const bodyInfo = bodies.get(body)
-        if (bodyInfo) {
-          if (bodyInfo.counter === 1) {
-            bodies.delete(body)
-            if (isIosDevice) {
-              body.ontouchmove = null
-
-              if (documentListenerAdded) {
-                off(document, 'touchmove', preventDefault)
-                documentListenerAdded = false
-              }
-            } else {
-              body.style.overflow = bodyInfo.initialOverflow
-            }
-          } else {
-            bodies.set(body, {
-              counter: bodyInfo.counter - 1,
-              initialOverflow: bodyInfo.initialOverflow,
-            })
-          }
-        }
-      }
-
-      useEffect(() => {
-        const body = getClosestBody(elementRef!.current)
-        if (!body) {
-          return
-        }
-        if (locked) {
-          lock(body)
-        } else {
-          unlock(body)
-        }
-      }, [locked, elementRef.current])
-
-      // clean up, on un-mount
-      useEffect(() => {
-        const body = getClosestBody(elementRef!.current)
-        if (!body) {
-          return
-        }
-        return () => {
-          unlock(body)
-        }
-      }, [])
     }
+
+    useEffect(() => {
+      const body = getClosestBody(elementRef!.current)
+      if (!body) {
+        return
+      }
+      if (locked) {
+        lock(body)
+      }
+      else {
+        unlock(body)
+      }
+    }, [locked, elementRef.current])
+
+    // clean up, on un-mount
+    useEffect(() => {
+      const body = getClosestBody(elementRef!.current)
+      if (!body) {
+        return
+      }
+      return () => {
+        unlock(body)
+      }
+    }, [])
+  }
